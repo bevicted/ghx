b23aadfc631a15331911166c6a1b65e7172cd853
bevicted <170766675+bevicted@users.noreply.github.com>
2024-06-08 11:40:49 +0200

    WIP: half-baked tests
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- issues_test.go
+++ issues_test.go
@@ -202,3 +202,105 @@ func TestMapSearchIssues(t *testing.T) {
 		})
 	}
 }
+
+func TestSearchOneIssue(t *testing.T) {
+	t.Parallel()
+
+	testCtx := context.Background()
+
+	for _, tc := range []struct {
+		name             string
+		searchQualifiers SearchQualifiers
+		issuesOnPages    []int
+		lastPageErr      error
+		handlerErr       error
+		expectIssues     int
+		expectErr        error
+		mockIssue        *github.Issue
+	}{
+		{
+			name: "ok - 0 issues",
+		},
+		{
+			name: "ok - 1 page, 1 issue",
+			searchQualifiers: SearchQualifiers{
+				IsIssue,
+				IsOpen,
+				RestrictTextSearchToBody,
+				HasText("some very specific text"),
+			},
+			issuesOnPages: []int{1},
+			expectIssues:  1,
+		},
+		{
+			name: "ok - 3 pages, 15 issues",
+			searchQualifiers: SearchQualifiers{
+				IsIssue,
+				IsOpen,
+				RestrictTextSearchToBody,
+			},
+			issuesOnPages: []int{5, 5, 5},
+			expectIssues:  15,
+		},
+		{
+			name: "err - 1 page, repoIssueListerErr on page 1",
+			searchQualifiers: SearchQualifiers{
+				IsIssue,
+				IsOpen,
+				RestrictTextSearchToBody,
+			},
+			lastPageErr: errors.New("repoIssueListerErr"),
+			expectErr:   errors.New("repoIssueListerErr"),
+		},
+		{
+			name: "err - 3 pages, 10 issues, repoIssueListerErr on page 3",
+			searchQualifiers: SearchQualifiers{
+				IsIssue,
+				IsOpen,
+				RestrictTextSearchToBody,
+			},
+			lastPageErr:   errors.New("repoIssueListerErr"),
+			issuesOnPages: []int{5, 5, 0},
+			expectIssues:  10,
+			expectErr:     errors.New("repoIssueListerErr"),
+		},
+		{
+			name: "err - 2 pages, handlerErr fails before repoIssueListerErr",
+			searchQualifiers: SearchQualifiers{
+				IsIssue,
+				IsOpen,
+				RestrictTextSearchToBody,
+			},
+			lastPageErr:   errors.New("repoIssueListerErr"),
+			issuesOnPages: []int{5, 0},
+			handlerErr:    errors.New("handlerErr"),
+			expectErr:     errors.New("handlerErr"),
+		},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockIssueSearcherTestFunc := func(t *testing.T, ctx context.Context, query string, searchOptions *github.SearchOptions) {
+				assert.Equal(t, testCtx, ctx)
+				assert.Equal(t, tc.searchQualifiers.String(), query)
+				assert.Equal(t, github.SearchOptions{ListOptions: github.ListOptions{}}, *searchOptions)
+			}
+			issueSearcher := &ghxtest.MockIssueSearcher{
+				T:        t,
+				TestFunc: mockIssueSearcherTestFunc,
+				ReturnValues: []*ghxtest.MockIssueSearcherReturnValues{
+					{
+						IssueSearchResult: &github.IssuesSearchResult{
+							Issues: []*github.Issue{tc.mockIssue},
+						},
+					},
+				},
+			}
+
+			issue, err := SearchOneIssue(testCtx, issueSearcher, tc.searchQualifiers)
+			assert.Equal(t, tc.mockIssue, issue)
+			assert.Equal(t, tc.expectErr, err)
+		})
+	}
+}
